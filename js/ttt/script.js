2ee549bb442b37c249fdfe1889ec7213df35fc82
unknown <ml43@protonmail.com>
2019-04-07 06:17:16 +0800

    Add script.js

--- js/ttt/script.js
+++ js/ttt/script.js
@@ -1,12 +1,35 @@
-td{
-	border: 2px solid #333;
-	height: 100px;
-	width: 100px;
-	text-align: center;
-	vertical-align: middle;
-	font-family: "Comic Sans MS"
+var origBoard;
+const huPlayer = 'O';
+const aiPlayer = 'X';
+const winCombos = [
+	[0, 1, 2],
+	[3, 4, 5],
+	[6, 7, 8],
+	[0, 3, 6],
+	[1, 4, 7],
+	[2, 5, 8],
+	[0, 4, 8],
+	[6, 4, 2]
+]
 
+const cells = document.querySelectorAll('.cell'); 
+startGame(); // function to start game
 
+function startGame() {
+	document.querySelector(".endgame").style.display = "none"; // when hit replay will be set to none
+	origBoard = Array.from(Array(9).keys()); 
+	for (var i = 0; i < cells.length; i++) {  //go through cells 
+		cells[i].innerText = '';
+		cells[i].style.removeProperty('background-color');
+		cells[i].addEventListener('click', turnClick, false);
+	}
+}
 
+function turnClick(square) {
+	turn(square.target.id, huPlayer)
+}
 
-} 
+function turn(squareId, player) {
+	origBoard[squareId] = player;
+	document.getElementById(squareId).innerText = player;
+}
